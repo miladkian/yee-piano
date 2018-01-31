55b149d4e61c45ceb82e8b5220d30df7eb1e479a
Milad Kianmehr <milad.kian@gmail.com>
2018-02-01 00:21:04 +0330

    log notes

---
+++ index.js
@@ -0,0 +1,41 @@
+var midi = require('midi');
+
+// Set up a new input.
+var input = new midi.input();
+
+// Count the available input ports.
+input.getPortCount();
+
+// Get the name of a specified input port.
+input.getPortName(0);
+
+// Configure a callback.
+input.on('message', function(deltaTime, message) {
+  // The message is an array of numbers corresponding to the MIDI bytes:
+  //   [status, data1, data2]
+  // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
+  // information interpreting the messages.
+  if(message != '254'){
+  	if(message[2] != 0)
+ console.log('m:' + message[2] );
+ }
+  
+ 
+});
+
+// Open the first available input port.
+input.openPort(0);
+
+// Sysex, timing, and active sensing messages are ignored
+// by default. To enable these message types, pass false for
+// the appropriate type in the function below.
+// Order: (Sysex, Timing, Active Sensing)
+// For example if you want to receive only MIDI Clock beats
+// you should use
+// input.ignoreTypes(true, false, true)
+input.ignoreTypes(true, true, true);
+
+// ... receive MIDI messages ...
+
+// Close the port when done.
+//input.closePort();
